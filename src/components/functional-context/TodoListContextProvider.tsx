6c927f21348e21f000a9d0d52ce6b52ea449afc5
Alan Shelly <alan.m.shelly@gmail.com>
2020-04-02 12:02:06 +0900

    Refactors functional-context dispatch to use custom hook

--- src/components/functional-context/TodoListContextProvider.tsx
+++ src/components/functional-context/TodoListContextProvider.tsx
@@ -1,13 +1,4 @@
-import React, {
-    createContext,
-    Dispatch,
-    FunctionComponent,
-    ReactNode,
-    SetStateAction,
-    useCallback,
-    useEffect,
-    useState,
-} from 'react'
+import React, {createContext, FunctionComponent, ReactNode, useCallback, useEffect, useState} from 'react'
 import {TodoItem, TodoRepo} from '../../TodoRepo'
 
 export const TodoListContext = createContext<{ todoItems: TodoItem[], dispatch: (action: TodoItemsReducerAction) => void }>({
@@ -19,32 +10,6 @@ type TodoItemsReducerAction =
     { type: 'add', text: string } |
     { type: 'load' } |
     { type: 'delete', id: number }
-
-
-function createDispatchFunction(todoRepo: TodoRepo, updateTodoItems: Dispatch<SetStateAction<TodoItem[]>>) {
-    return function dispatch(action: TodoItemsReducerAction) {
-        (async () => {
-            switch (action.type) {
-                case 'add':
-                    const addedItem = await todoRepo.addItem(action.text)
-                    updateTodoItems(todoItems =>
-                        [...todoItems, addedItem],
-                    )
-                    return
-                case 'load':
-                    const loadedTodoItems = await todoRepo.getItems()
-                    updateTodoItems(loadedTodoItems)
-                    return
-                case 'delete':
-                    await todoRepo.deleteItem(action.id)
-                    updateTodoItems(todoItems =>
-                        todoItems.filter(todoItem => todoItem.id !== action.id),
-                    )
-                    return
-            }
-        })()
-    }
-}
 
 interface Props {
     todoRepo: TodoRepo
@@ -61,14 +26,9 @@ export const TodoListContextProvider: FunctionComponent<Props> = (props: Props) 
     const [todoItems, updateTodoItems] = useState<TodoItem[]>([])
 
     /* NOTE:
-     * I used useCallback to generate the dispatch function as otherwise the dispatch function will be recreated every
-     * time todoItems is updated. This in turn lets me only call dispatch the first time the component loads using the
-     * useEffect hook.
+     * This is a custom hook
      */
-    const dispatch = useCallback(
-        createDispatchFunction(todoRepo, updateTodoItems),
-        [todoRepo, updateTodoItems],
-    )
+    const dispatch = useTodoDispatch(todoRepo, updateTodoItems)
 
     /* NOTE:
      * You can name hook callbacks for readability :)
@@ -81,5 +41,42 @@ export const TodoListContextProvider: FunctionComponent<Props> = (props: Props) 
         <TodoListContext.Provider value={{todoItems, dispatch}}>
             {children}
         </TodoListContext.Provider>
+    )
+}
+
+function useTodoDispatch(todoRepo: TodoRepo, updateTodoItems: (value: (((prevState: TodoItem[]) => TodoItem[]) | TodoItem[])) => void) {
+    /* NOTE:
+     * I used useCallback to generate the dispatch function as otherwise the dispatch function will be recreated every
+     * time todoItems is updated. This in turn lets me only call dispatch the first time the component loads using the
+     * useEffect hook.
+     */
+    return useCallback(
+        function dispatch(action: TodoItemsReducerAction) {
+            switch (action.type) {
+                case 'add':
+                    todoRepo.addItem(action.text)
+                        .then(addedItem => {
+                            updateTodoItems(todoItems =>
+                                [...todoItems, addedItem],
+                            )
+                        })
+                    break
+                case 'load':
+                    todoRepo.getItems()
+                        .then(loadedTodoItems => {
+                            updateTodoItems(loadedTodoItems)
+                        })
+                    break
+                case 'delete':
+                    todoRepo.deleteItem(action.id)
+                        .then(() => {
+                            updateTodoItems(todoItems =>
+                                todoItems.filter(todoItem => todoItem.id !== action.id),
+                            )
+                        })
+                    break
+            }
+        },
+        [todoRepo, updateTodoItems],
     )
 }
